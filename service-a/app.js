b2175b44b9b1e507dd72812529e5e8c2a816efd5
deepenc <sunilojha525@gmail.com>
2025-08-01 00:24:54 +0545

    testing

--- service-a/app.js
+++ service-a/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Service A-testing');
+  res.send('Hello from Service A');
 });
 
 app.listen(port, () => {
