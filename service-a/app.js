036c8d1df08875a808ee392ffabd1de8740f3804
deepenc <sunilojha525@gmail.com>
2025-08-14 12:23:18 +0545

    testing

--- service-a/app.js
+++ service-a/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('This is Service A- testing for CI part');
+  res.send('This is Service A-testing');
 });
 
 app.listen(port, () => {
